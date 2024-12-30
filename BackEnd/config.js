dc5e9ab62f1d8074892e05dd1077107ae12b08cd
tanishk2612 <tanishkwrs28@gmail.com>
2024-12-31 01:39:43 +0530

    first commit

---
+++ BackEnd/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://root:root@books-library-mern.r3ctr.mongodb.net/Books-Collection?retryWrites=true&w=majority&appName=Books-Library-MERN'
